35a81c24927ad1ec55b69917e0654e2e962f6b44
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-13 15:19:22 +0900

    Auth 기능 만들기
    auth 는 로그인/비로그인/관리자를 나눌 수 있는 기능이라고 볼 수 있다

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { User } = require("./models/User");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
+const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/x-www-form-urlencoded 이런 데이터를 분석(parsing)해서 가져옴
@@ -71,6 +72,22 @@ app.post("/api/users/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authenrication이 True 라는말
+
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    //role이 0이면 일반유저 0이 아니면 관리자
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
