63a2ddac6619a8bc023e670a5e1b08ebaaf37914
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-15 23:15:16 +0900

    CORS & Proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
@@ -99,6 +98,8 @@ app.get("/api/users/logout", auth, (req, res) => {
     });
   });
 });
+
+const port = 5000;
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
